20ce6a422f0e8592d4e6bbf48e586505d54cf779
Janeks111 <90975618+Janeks111@users.noreply.github.com>
2023-11-10 20:09:16 +0000

    Added all necesary links to HTML and made fuction to diplay current day and to color code the blcks

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  // Display current day using dayjs
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+  // Function to color-code time blocks
+  function updateHourlyBlocks() {
+    var currentHour = dayjs().hour();
+
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("data-hour"));
+
+      if (blockHour < currentHour) {
+        $(this).removeClass("future present").addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past future").addClass("present");
+      } else {
+        $(this).removeClass("past present").addClass("future");
+      }
+    });
+  }
+});
